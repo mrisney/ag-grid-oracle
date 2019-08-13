a60bcc86eb7439c984af63626e24d4a43ed80e89
mrisney <marc.risney@gmail.com>
2019-08-13 01:32:38 -0700

    update

--- src/main/java/com/itis/ag/grid/oracle/config/WebConfig.java
+++ src/main/java/com/itis/ag/grid/oracle/config/WebConfig.java
@@ -16,11 +16,5 @@ public class WebConfig implements WebMvcConfigurer {
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedHeaders("*");
 	}
 
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-	}
 	
 }
